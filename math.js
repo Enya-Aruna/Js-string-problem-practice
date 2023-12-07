7c0aff3415585af20aff83ccdaa07221b522030f
enya <salmaislam489265@gmail.com>
2023-12-07 08:39:57 +0600

    math pow, ads

---
+++ math.js
@@ -0,0 +1,14 @@
+const result = Math.pow(2,4);
+//console.log(result)
+
+const num1 = 25 ;
+const num2 = 50 ;
+
+const gap = Math.abs(num1-num2) ;
+console.log(gap)
+if(gap<5){
+    console.log('you guys can be friends')
+}
+else{
+    console.log('you guys stay apart');
+}

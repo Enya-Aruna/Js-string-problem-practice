b7e3464cf8cdd06e1d741c1f9684ee43945b8548
enya <salmaislam489265@gmail.com>
2023-12-07 08:56:03 +0600

    round , ceil, floor

--- math.js
+++ math.js
@@ -5,10 +5,19 @@ const num1 = 25 ;
 const num2 = 50 ;
 
 const gap = Math.abs(num1-num2) ;
-console.log(gap)
+//console.log(gap)
 if(gap<5){
-    console.log('you guys can be friends')
+    // console.log('you guys can be friends')
 }
 else{
-    console.log('you guys stay apart');
+    // console.log('you guys stay apart');
 }
+ const number = 2.567 ;
+ const fullNumber = Math.round(number);
+ console.log(fullNumber)
+
+ const result1 = Math.ceil(2.0001)
+ console.log(result1)
+
+ const result2 = Math.floor(456.245)
+ console.log(result2)
